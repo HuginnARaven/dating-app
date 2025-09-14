37cf8ec5da26f12360d69cf53c6c5824bfeb2ef3
Artem <ravennhuginn@gmail.com>
2025-09-14 13:13:56 +0300

    Error handling commit:
    -added controller in api and page in client for testing different types of errors
    -creted middleware in api for handling errors
    -created interceptor in client for handling errors
    -created pages for 404 and 500 erros in client

---
+++ client/src/core/interceptors/error-interceptor.ts
@@ -0,0 +1,47 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError } from 'rxjs';
+import { ToastService } from '../services/toast-service';
+import { NavigationExtras, Router } from '@angular/router';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const toast = inject(ToastService);
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError((err) => {
+      if (err) {
+        switch (err.status) {
+          case 400:
+            if (err.error.errors) {
+              const modelStateErrors = [];
+              for (const key in err.error.errors) {
+                if (err.error.errors[key]) {
+                  modelStateErrors.push(err.error.errors[key]);
+                }
+              }
+              throw modelStateErrors.flat();
+            } else {
+              toast.error(err.error);
+            }
+            break;
+          case 401:
+            toast.error('Unauthorized');
+            break;
+          case 404:
+            router.navigateByUrl('/not-found');
+            break;
+          case 500:
+            const navigationExtras: NavigationExtras = {state: {error: err.error}}
+            router.navigateByUrl('/server-error', navigationExtras)
+            break;
+          default:
+            toast.error('Smth went wrong');
+            break;
+        }
+      }
+
+      throw err;
+    })
+  );
+};
