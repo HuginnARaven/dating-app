4e7edccf7065a73ffa9c78c4a2ca6e0003ae8523
Artem <ravennhuginn@gmail.com>
2025-09-18 21:16:08 +0300

    Register form update commit(API/client):
    -API:
        -modified api/account/register/ endpoint, so it now creates Member alongside AppUser and accepts new fields that are needed for Member creation
    -client:
        -changed register form to be reactive and "made it two-step form" and added some validations to it
        -creted custom reusable text-input to reduce amount of repeating code in forms
        -fixed a bug that happened when user uploaded first image(profile picture was not updated on the client side)

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,27 +1,88 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RegisterCreds, User } from '../../../types/user';
+import { Component, inject, output, signal } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { RegisterCreds } from '../../../types/user';
 import { AccountService } from '../../../core/services/account-service';
+import { JsonPipe } from '@angular/common';
+import { TextInput } from "../../../shared/text-input/text-input";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, TextInput],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
   private accountService = inject(AccountService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder)
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
+  protected credentialsForm: FormGroup;
+  protected profileForm: FormGroup;
+  protected currentStep = signal(1);
+  protected validationErrors = signal<string[]>([]);
+
+  constructor() {
+    this.credentialsForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      displayName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(12)]],
+      confirmPassword: ['', [Validators.required, this.matchValue('password')]],
+    });
+
+    this.profileForm = this.fb.group({
+      gender: ['male', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+    })
+
+    this.credentialsForm.controls['password'].valueChanges.subscribe(() => {
+      this.credentialsForm.controls['confirmPassword'].updateValueAndValidity();
+    });
+
+  }
+
+  matchValue(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const parent= control.parent;
+      if (!parent) return null;
+      const matchValue = parent.get(matchTo)?.value;
+      return control.value === matchValue ? null : {passwordMismatch: true}
+    }
+  }
+
+  nextStep(){
+    if (this.credentialsForm.valid){
+      this.currentStep.update(prevStep => prevStep + 1);
+    }
+  }
+
+  prevStep(){
+    this.currentStep.update(prevStep => prevStep - 1);
+  }
+
+  getMaxDate(){
+    const today = new Date();
+    today.setFullYear(today.getFullYear() - 18)
+    return today.toISOString().split('T')[0];
+  }
 
   register(){
-    this.accountService.register(this.creds).subscribe({
-      next: res => {
-        console.log(res);
-        this.cancel();
+    if(this.profileForm.valid && this.credentialsForm.valid){
+      const formData = {...this.credentialsForm.value, ...this.profileForm.value}
+
+      this.accountService.register(formData).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/members');
       },
-      error: err => console.log(err)
+      error: err => {
+        console.log(err);
+        this.validationErrors.set(err);
+      }
     })
+    }
   }
 
   cancel(){
--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -2,68 +2,77 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Member, Photo } from '../../../types/member';
 import { MemberService } from '../../../core/services/member-service';
-import { ImageUpload } from "../../../shared/image-upload/image-upload";
+import { ImageUpload } from '../../../shared/image-upload/image-upload';
 import { AccountService } from '../../../core/services/account-service';
 import { User } from '../../../types/user';
-import { StarButton } from "../../../shared/star-button/star-button";
-import { DeleteButton } from "../../../shared/delete-button/delete-button";
+import { StarButton } from '../../../shared/star-button/star-button';
+import { DeleteButton } from '../../../shared/delete-button/delete-button';
 
 @Component({
   selector: 'app-member-photos',
   imports: [ImageUpload, StarButton, DeleteButton],
   templateUrl: './member-photos.html',
-  styleUrl: './member-photos.css'
+  styleUrl: './member-photos.css',
 })
 export class MemberPhotos implements OnInit {
   protected memberService = inject(MemberService);
   protected accoundService = inject(AccountService);
-  private route = inject(ActivatedRoute)
+  private route = inject(ActivatedRoute);
   protected photos = signal<Photo[]>([]);
   protected loading = signal(false);
 
   ngOnInit(): void {
     const membetId = this.route.parent?.snapshot.paramMap.get('id');
-    if (membetId){
+    if (membetId) {
       this.memberService.getMemberPhotos(membetId).subscribe({
-        next: photos => this.photos.set(photos)
+        next: (photos) => this.photos.set(photos),
       });
     }
   }
 
-  onUploadImage(file: File){
+  onUploadImage(file: File) {
     this.loading.set(true);
     this.memberService.uploadPhoto(file).subscribe({
-      next: photo => {
+      next: (photo) => {
         this.memberService.eidtMode.set(false);
         this.loading.set(false);
-        this.photos.update(photos => [...photos, photo])
+        this.photos.update((photos) => [...photos, photo]);
+        if(!this.memberService.member()?.imageUrl){
+          this.setMainPhotoLocal(photo);
+        } 
       },
-      error: err => {
+      error: (err) => {
         console.log('Error uploading image: ', err);
         this.loading.set(false);
-      }
-    })
+      },
+    });
   }
 
-  setMainPhoto(photo: Photo){
+  setMainPhoto(photo: Photo) {
     this.memberService.setMainPhoto(photo).subscribe({
       next: () => {
-        const currentUsser = this.accoundService.currentUser();
-        if (currentUsser) currentUsser.imageUrl = photo.url;
-        this.accoundService.setCurrentUser(currentUsser as User);
-        this.memberService.member.update(member => ({
-          ...member,
-          imageUrl: photo.url
-        }) as Member)
-      }
-    })
+        this.setMainPhotoLocal(photo);
+      },
+    });
   }
 
-  deletePhoto(photo: Photo){
+  deletePhoto(photo: Photo) {
     this.memberService.deletePhto(photo).subscribe({
       next: () => {
-        this.photos.update(photos => photos.filter(x => x.id !== photo.id))
-      }
-    })
+        this.photos.update((photos) => photos.filter((x) => x.id !== photo.id));
+      },
+    });
+  }
+
+  private setMainPhotoLocal(photo: Photo) {
+    const currentUser = this.accoundService.currentUser();
+    if (currentUser) currentUser.imageUrl = photo.url;
+    this.accoundService.setCurrentUser(currentUser as User);
+    this.memberService.member.update((member) => 
+      ({
+          ...member,
+          imageUrl: photo.url,
+        } as Member)
+    );
   }
 }
